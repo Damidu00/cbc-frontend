e1b5a0ce0c34f30de9b8ab2e8eba663d4d6413e4
Damidu00 <154224349+Damidu00@users.noreply.github.com>
2024-12-25 20:44:17 +0530

    created paths with routes 😁

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Testing from './components/testing'
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
+import HomePage from './pages/homePage'
+import LoginPage from './pages/loginPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Testing/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage/>} />
+          <Route path='/login' element={<LoginPage/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }

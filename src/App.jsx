a8d0ece9f11f5e07b913b39ea5bd7863534ea20a
Damidu00 <154224349+Damidu00@users.noreply.github.com>
2024-12-26 22:13:51 +0530

    admin page sketch corded 😮‍💨

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
 import HomePage from './pages/homePage'
 import LoginPage from './pages/loginPage'
+import AdminHomePage from './pages/adminHomePage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,7 @@ function App() {
           <Route path='/' element={<HomePage/>} />
           <Route path='/login' element={<LoginPage/>}/>
           <Route path='/*' element={<h1>404 error not found</h1>} />
+          <Route path='/admin' element={<AdminHomePage/>}/>
         </Routes>
       </BrowserRouter>
     </>

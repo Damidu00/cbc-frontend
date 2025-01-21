46c470a52f2e49aac02d3686b8b79f563831aa12
Damidu00 <154224349+Damidu00@users.noreply.github.com>
2025-01-21 16:38:10 +0530

    creating productinfo page and image slide show

--- src/pages/homePages/productInfo.jsx
+++ src/pages/homePages/productInfo.jsx
@@ -57,19 +57,23 @@ export default function ProductInfo() {
             status == "found" && (
                 <div className='w-full h-full flex items-center justify-center'>
                     {/* image part */}
-                    <div className='w-[40%] h-full flex items-center justify-center' >
-                        <div className='w-[100%] h-[calc(100vh-200px)] p-5 '>
-                            <img src={product.images[0]} alt="" className='w-full h-full object-cover'/>
+                    <div className='w-[40%] h-full flex items-center justify-center ml-5' >
+                        <div className='w-[100%] h-[calc(100vh-150px)] p-5 '>
+                            <img src={product.images[0]} alt="" className='w-full  object-cover'/>
                         </div>
                     </div>
                     {/* details part */}
                     <div className='w-[60%] h-full  flex items-center justify-center'>
-                        <div className='w-[1000%] h-[calc(100vh-200px)] p-5 '>
+                        <div className='w-[1000%] h-[calc(100vh-100px)] p-5 '>
                             <h1 className='font-semibold text-gray-950 text-[40px] font-sans '>{product.productName}</h1>
                             <h1 className='font-semibold text-gray-400 text-[20px] font-serif '>{product.altNames.join(" | ")}</h1>
-                            <div className='flex flex-row gap-10 items-center'>
-                                <p className='text-red-600 font-semibold font-serif text-[30px] ' ><span className='text-[25px]'>Rs: </span>{product.lastPrice}</p>
-                                <p className='text-gray-600 font-semibold font-serif text-[20px]' ><span className='text-[15px]'>Rs: </span>{product.price}</p>
+                            <div className='flex flex-row gap-5 items-center'>
+                                {
+                                    (product.price > product.lastPrice)&&
+                                    <p className='text-gray-600 font-semibold font-mono text-[20px] line-through' >{"LKR:" + product.price}</p>                                    
+                                }
+                                <p className='text-red-600 font-semibold font-mono text-[35px] ' >{"Rs:" + product.lastPrice}</p>
+                                                              
                             </div> 
                         </div>
                     </div>

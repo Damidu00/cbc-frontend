70ee567d159cd8db4926bb512833235a1c448ab1
Damidu00 <154224349+Damidu00@users.noreply.github.com>
2025-01-22 22:20:45 +0530

    tested axios req of products

--- src/pages/homePages/products.jsx
+++ src/pages/homePages/products.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NavigationBar from '../../components/navigationBar'
+import axios from 'axios'
+import toast from 'react-hot-toast'
 
 export default function Products() {
+
+  const [products,setProducts] = useState([])
+  const [loadingStatus,setloadingStatus]= useState('loading')
+
+  useEffect(()=>{
+    if(loadingStatus == "loading"){
+      axios.get(import.meta.env.VITE_BACKEND_URL + `/api/products`).then((res)=>{
+        console.log(res.data)
+        setProducts(res.data)
+      }).catch(
+        (err)=>toast.error('Error fetching products')
+      )
+    }
+    
+  }
+  ,[])
+
   return (
     <>
     <NavigationBar/>

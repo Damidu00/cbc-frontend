9122235ba8202270c8d164e3b1a3c23cbc86cc52
Damidu00 <154224349+Damidu00@users.noreply.github.com>
2025-02-06 00:35:07 +0530

    added coustomer profilefor the feedback component

--- src/components/feedbackCart.jsx
+++ src/components/feedbackCart.jsx
@@ -4,7 +4,13 @@ export default function FeedbackCart({ feedback }) {
   return (
     <div className='flex justify-center p-4'>
       <div className='w-full max-w-xl bg-white shadow-md rounded-xl p-6 border border-gray-200'>
-        <h1 className='text-2xl font-semibold text-black mb-4'>Customer Feedback</h1>
+        <h1 className='text-3xl font-semibold text-black mb-4'>Customer Feedback</h1>
+        
+        <div className='flex items-center gap-3 '>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4wTQySWL01Oo0Clt2jBav69Hxl5QjpNLbsQ&s" className='w-[20px] h-[20px]' />
+          <p className='text-gray-600 font-semibold text-[15px]'>{feedback.user_name}</p>
+        </div>
+        
         <p className='text-gray-900'>{feedback.message || "No message provided."}</p>
 
         <div className='mt-6 p-4 bg-gray-100 rounded-lg'>
--- src/pages/homePages/addFeedback.jsx
+++ src/pages/homePages/addFeedback.jsx
@@ -1,22 +1,63 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
 
 export default function AddFeedback() {
+
+  const [message, setMessage] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const feedback = { message };
+    const token = localStorage.getItem("token");
+
+    console.log("Token - "+token);
+
+    axios.post(import.meta.env.VITE_BACKEND_URL + "/api/feedbacks/", feedback,{
+      headers: {
+        Authorization: "Bearer "+token
+      }
+    })
+      .then(() => {
+        toast.success("Feedback sent successfully!");
+        navigate("/feedbacks");
+      })
+      .catch((error) => {
+        console.error("Error submitting feedback:", error);
+        toast.error("Error submitting feedback.");
+      });
+  };
+
   return (
     <div className="min-h-screen bg-gray-200 flex items-center justify-center p-4 -mt-5">
       <div className="bg-white shadow-2xl rounded-2xl p-8 max-w-lg w-full text-center">
-        <h2 className="text-3xl font-bold text-blue-500 ">We Value Your Feedback</h2>
+        <h2 className="text-3xl font-bold text-blue-600">We Value Your Feedback</h2>
         <p className="text-gray-700 mt-4">
-          At Cristal Beauty Clear, we take pride in providing exceptional beauty products and services to enhance your confidence and charm. Your satisfaction is our priority, and we strive for excellence in everything we do. However, if we ever fall short, we sincerely apologize. Your feedback helps us improve and serve you better. Please share your thoughts below—we truly value your voice.
+          At Cristal Beauty Clear, we take pride in providing exceptional beauty products and services to enhance your confidence and charm.
+          Your satisfaction is our priority, and we strive for excellence in everything we do. However, if we ever fall short, we sincerely apologize.
+          Your feedback helps us improve and serve you better. Please share your thoughts below—we truly value your voice.
         </p>
-        <form className="mt-6">
-          <textarea 
+        <form className="mt-6" onSubmit={handleSubmit}>
+          {/* <input
+            type="text"
+            value={userName}
+            placeholder="Enter Your User Name"
+            className="h-10 w-full mb-2 px-4 border border-gray-300 rounded-lg focus:ring-2 focus:ring-black focus:outline-none shadow-sm transition duration-300"
+            onChange={(e) => setUserName(e.target.value)}
+          /> */}
+          <textarea
+            value={message}
             className="w-full p-4 border border-gray-300 rounded-lg focus:ring-2 focus:ring-black focus:outline-none"
             rows="5"
             placeholder="Write your feedback here..."
+            onChange={(e) => setMessage(e.target.value)}
           ></textarea>
-          <button 
-            type="submit" 
-            className="mt-4 bg-black  text-white py-2 px-6 rounded-full shadow-md hover:bg-gray-400 hover:text-black transition duration-300"
+          <button
+            type="submit"
+            className="mt-4 bg-black text-white py-2 px-6 rounded-full shadow-md hover:bg-gray-400 hover:text-black transition duration-300"
           >
             Submit Feedback
           </button>

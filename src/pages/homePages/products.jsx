28a38c3bb3cbed9c08ed08210ee9e944b02b4188
Damidu00 <154224349+Damidu00@users.noreply.github.com>
2025-03-01 19:59:24 +0530

    product search function done🫡😮‍💨

--- src/pages/homePages/products.jsx
+++ src/pages/homePages/products.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import NavigationBar from '../../components/navigationBar'
 import axios from 'axios'
 import toast from 'react-hot-toast'
 import ProductCard from '../../components/productCard'
@@ -8,7 +7,7 @@ export default function Products() {
 
   const [products,setProducts] = useState([])
   const [loadingStatus,setloadingStatus]= useState('loading')
-
+  const [query, setQuery] = useState("");
   useEffect(()=>{
     if(loadingStatus == "loading"){
       axios.get(import.meta.env.VITE_BACKEND_URL + `/api/products`).then((res)=>{
@@ -23,8 +22,42 @@ export default function Products() {
   }
   ,[])
 
+  function search(e) {
+    const query = e.target.value;
+    setQuery(query);
+    setloadingStatus("loading");
+    if (query == "") {
+      axios
+        .get(import.meta.env.VITE_BACKEND_URL + "/api/products")
+        .then((res) => {
+          console.log(res.data);
+          setProducts(res.data);
+          setloadingStatus("loaded");
+        })
+        .catch((err) => toast.error("Error loading products"));
+    }else{
+      axios
+        .get(import.meta.env.VITE_BACKEND_URL + "/api/products/search/"+query)
+        .then((res) => {
+          console.log(res.data);
+          setProducts(res.data);
+          setloadingStatus("loaded");
+        })
+        .catch((err) => toast.error("Error loading products"));
+    }
+  }
+
   return (
     <div className=' h-full flex flex-wrap justify-center overflow-hidden '>
+
+      <div className=" w-full flex justify-center m-2 absolute sticky mb-10 ">
+        <input type="text" className="w-1/2 p-2 absolute z-50 border- bg-gray-200 rounded-full flex text-center  "
+            placeholder="Search Products"
+            onChange={search}
+            alue={query}
+        />
+      </div>
+
       {
         products.map((product)=>
           <ProductCard key={product.id || product._id}  product={product}/>

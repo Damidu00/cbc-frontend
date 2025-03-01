5fafb8925bf62c62420d736e9ea66ba0d493af82
Damidu00 <154224349+Damidu00@users.noreply.github.com>
2025-03-01 23:14:33 +0530

    admin page finalized

--- src/pages/adminHomePage.jsx
+++ src/pages/adminHomePage.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link, Route, Routes } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, Route, Routes, useNavigate } from 'react-router-dom'
 import { BsGraphDown } from "react-icons/bs"
 import { AiOutlineProduct } from "react-icons/ai"
 import { FaShoppingCart } from "react-icons/fa"
@@ -11,11 +11,44 @@ import UpdateProduct from './adminPages/updateProduct'
 import Feedback from './adminPages/feedback'
 import ProductFeedbacks from './adminPages/productFeedbacks'
 import AdminOrdersPage from './adminPages/adminOrderpage'
+import axios from 'axios'
+import toast from 'react-hot-toast'
 
 export default function AdminHomePage() {
+  const navigate = useNavigate();
+
+  const [user,setUser] = useState(null)
+  useEffect(()=>{
+    const token = localStorage.getItem("token")
+    if (!token){
+      return;
+      navigate("/login")
+    }
+    axios.get(import.meta.env.VITE_BACKEND_URL + "/api/users",{
+      headers : {
+        Authorization : `Bearer ${token}`
+      },
+    }).then((res)=>{
+      console.log(res.data)
+      if(res.data.type != 'admin'){
+        toast.error("Unauthorized access")
+        navigate("/login")
+      }else{
+        setUser(res.data)
+        navigate("/admin")
+      }
+
+    }).catch((err)=>{
+      console.log(err)
+      toast.error("Failed to fetch user data")
+      navigate("/login")
+    })
+    
+  },[])
+
   return (
     <div className='bg-blue-300 w-full h-screen flex'>
-        <div className='w-[20%] h-screen bg-blue-900 flex flex-col items-center ' >
+        <div className='w-[20%] h-screen bg-gray-900 flex flex-col items-center ' >
             <Link className='m-2 flex flex-row items-center text-white text-lg hover:text-amber-600 hover:-translate-y-1 transition-transform duration-300 hover:scale-110' to="/admin/dashboard"><BsGraphDown className='mr-2' />Dashboard</Link>
             <Link className='m-2 flex flex-row items-center text-white text-lg hover:text-amber-600 hover:-translate-y-1 transition-transform duration-300 hover:scale-110' to="/admin/products"><AiOutlineProduct className='mr-2' />Products</Link>
             <Link className='m-2 flex flex-row items-center text-white text-lg hover:text-amber-600 hover:-translate-y-1 transition-transform duration-300 hover:scale-110' to="/admin/productFeedbacks"><AiOutlineProduct className='mr-2' />Product Fedbacks</Link>
@@ -25,7 +58,7 @@ export default function AdminHomePage() {
         </div>
 
         <div className="w-[80%] bg-gray-200 h-screen">
-            <Routes path="/*">
+            {user!==null && <Routes path="/*">
                 <Route path="/dashboard" element={<DashboardPage/>}/>
                 <Route path='/products' element={<Products/>}/>
                 <Route path='/products/addProduct' element={<AddProductForm/>}/>
@@ -33,7 +66,13 @@ export default function AdminHomePage() {
                 <Route path='/feedbacks' element={<Feedback/>}/>
                 <Route path='/productFeedbacks' element={<ProductFeedbacks/>}/>
                 <Route path='/orders' element={<AdminOrdersPage/>}/>
-            </Routes>
+            </Routes>}
+            
+            {
+              user==null && <div className="w-full h-full flex justify-center items-center">
+              <div className="w-[60px] h-[60px] border-[4px] border-gray-200 border-b-[#3b82f6] animate-spin rounded-full"></div>
+            </div>
+            }
         </div>
     </div>
   )
